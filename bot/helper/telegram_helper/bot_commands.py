e4194d2669b8804f0a64b5b5b9f83129b7d82e89
mallumovieleech <88672268+mallumovieleech@users.noreply.github.com>
2022-01-22 20:08:15 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -16,7 +16,7 @@ class _BotCommands:
         self.RmSudoCommand = 'rmsudo'
         self.PingCommand = 'ping'
         self.RestartCommand = 'restart'
-        self.StatsCommand = 'stat'
+        self.StatsCommand = 'stats'
         self.HelpCommand = 'help'
         self.LogCommand = 'log'
         self.SpeedCommand = 'speedtest'
